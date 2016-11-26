cf40453f0f51360a6643b842ebff1f8c73e92a9d
Chihhung <aaronchlin1025@gmail.com>
2016-11-25 22:18:44 -0700

    cl - can click add on menu bar at main page

--- app/src/main/java/com/example/sherrychuang/splitsmart/Activity/MainActivity.java
+++ app/src/main/java/com/example/sherrychuang/splitsmart/Activity/MainActivity.java
@@ -3,40 +3,43 @@ package com.example.sherrychuang.splitsmart.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
 import android.view.Menu;
 import android.view.ContextMenu;
+import android.view.MenuItem;
 import java.util.*;
+
 import com.example.sherrychuang.splitsmart.data.*;
+import com.example.sherrychuang.splitsmart.manager.*;
 
 import com.example.sherrychuang.splitsmart.R;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView event_list;
+    private ListView event_list_view;
     private ArrayAdapter adapter;
-    private String[] list_events;
+    private List<Event> events;
+    private String[] events_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        event_list = (ListView) findViewById(R.id.event_list);
-        list_events = new String[3];
-        list_events[0] = "Fuck";
-        list_events[1] = "Shit";
-        list_events[2] = "Damn";
+        event_list_view = (ListView) findViewById(R.id.event_list);
+        EventManager eventManager = ManagerFactory.getEventManager(this);
+        events = eventManager.getAllEvents();
+        events_name = new String[events.size()];
+        for(int i = 0; i < events.size(); i++) {
+            events_name[i] = events.get(i).getName();
+        }
 
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list_events);
-        event_list.setAdapter(adapter);
-        registerForContextMenu(event_list);
-        event_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, events_name);
+        event_list_view.setAdapter(adapter);
+        registerForContextMenu(event_list_view);
+        event_list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 CharSequence text = Integer.toString(i);
@@ -45,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        event_list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//        event_list_view.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 //            @Override
 //            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
 //                CharSequence text = list_events[i];
@@ -55,14 +58,14 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-        ImageButton addEvent= (ImageButton) findViewById(R.id.addEvent);
-        addEvent.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                Intent myIntent = new Intent(MainActivity.this, CreateEventPage.class);
-                MainActivity.this.startActivity(myIntent);
-            }
-
-        });
+//        ImageButton addEvent= (ImageButton) findViewById(R.id.addEvent);
+//        addEvent.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View view){
+//                Intent myIntent = new Intent(MainActivity.this, CreateEventPage.class);
+//                MainActivity.this.startActivity(myIntent);
+//            }
+//
+//        });
 
 //        Button btn= (Button) findViewById(R.id.sampleEvent);
 //        btn.setOnClickListener(new View.OnClickListener(){
@@ -76,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         if (v.getId() == R.id.event_list) {
             AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-            menu.setHeaderTitle(list_events[info.position]);
+            menu.setHeaderTitle(events_name[info.position]);
             menu.add("Edit");
             menu.add("Delete");
         }
@@ -84,5 +87,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+    public void clickMenuItem(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.addEvent) {
+            Intent myIntent = new Intent(MainActivity.this, CreateEventPage.class);
+            MainActivity.this.startActivity(myIntent);
+        }
+        return;
     }
 }
