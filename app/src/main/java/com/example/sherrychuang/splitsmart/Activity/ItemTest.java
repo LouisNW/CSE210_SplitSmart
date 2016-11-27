8c13c46494507604695ab84f6fef7f25a744a5c3
Chien-Han <chl590@eng.ucsd.edu>
2016-11-26 23:33:20 -0800

    Breakthrough at add item

---
+++ app/src/main/java/com/example/sherrychuang/splitsmart/Activity/ItemTest.java
@@ -0,0 +1,17 @@
+package com.example.sherrychuang.splitsmart.Activity;
+
+/**
+ * Created by alice on 11/26/16.
+ */
+
+public class ItemTest {
+    public boolean tax;
+    public String itemName;
+    public String price;
+
+    public ItemTest(boolean tax, String itemName, String price) {
+        this.tax = tax;
+        this.itemName = itemName;
+        this.price = price;
+    }
+}
