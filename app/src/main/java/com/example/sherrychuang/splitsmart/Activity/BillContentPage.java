9b0216ee996c21b1d348eb29b91dd6db9a180a59
Chien-Han <chl590@eng.ucsd.edu>
2016-11-26 23:33:20 -0800

    Breakthrough at add item

--- app/src/main/java/com/example/sherrychuang/splitsmart/Activity/BillContentPage.java
+++ app/src/main/java/com/example/sherrychuang/splitsmart/Activity/BillContentPage.java
@@ -1,6 +1,7 @@
 package com.example.sherrychuang.splitsmart.Activity;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +11,8 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.widget.Spinner;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 import com.example.sherrychuang.splitsmart.R;
 
@@ -18,11 +20,31 @@ import com.example.sherrychuang.splitsmart.R;
  * Created by sherrychuang on 11/15/16.
  */
 
-public class BillContentPage extends Activity{
+public class BillContentPage extends ListActivity{
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<ItemTest> listItems = new ArrayList<ItemTest>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    CustAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        setContentView(R.layout.bill_content_page_layout);
+        setContentView(R.layout.item_layout);
+
+        adapter=new CustAdapter(this, listItems);
+        ListView listView = (ListView) findViewById(android.R.id.list);
+        setListAdapter(adapter);
+
+        ItemTest newItem = new ItemTest(true, "Apple", "1");
+        adapter.add(newItem);
     }
+
+    /***
+    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+    public void addItems(View v) {
+        listItems.add(0.1, "");
+        adapter.notifyDataSetChanged();
+    }
+     ***/
 }
