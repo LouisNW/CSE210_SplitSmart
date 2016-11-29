90c775f9cba6936ecda3fc5a7123db212534991d
Chien-Han <chl590@eng.ucsd.edu>
2016-11-28 20:06:33 -0800

    Manager added

--- app/src/main/java/com/example/sherrychuang/splitsmart/Activity/BillPage.java
+++ app/src/main/java/com/example/sherrychuang/splitsmart/Activity/BillPage.java
@@ -13,18 +13,39 @@ import android.widget.Spinner;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.lang.String;
+import java.lang.Long;
+
 import com.example.sherrychuang.splitsmart.R;
+import com.example.sherrychuang.splitsmart.data.*;
+import com.example.sherrychuang.splitsmart.manager.*;
+
 
 /**
  * Created by sherrychuang on 11/15/16.
  */
 
 public class BillPage extends Activity {
+    private String billName;
+    private List<Person> person;
+    private List<String> personlist;
+    private String ownerName;
+    private PersonManager personManager;
+    private BillManager billManager;
+    private Map<String, Long> map;
+    private Event event;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         setContentView(R.layout.bill_page_layout);
+
         //Instantiates a layout XML file into the View object alertLayout
         LayoutInflater inflater = getLayoutInflater();
         View alertLayout = inflater.inflate(R.layout.bill_dialog, null);
@@ -35,6 +56,18 @@ public class BillPage extends Activity {
         alert.setTitle("Create a New Bill");
         //Disallow cancel of AlertDialog on click of back button and outside touch
         alert.setCancelable(false);
+
+        /***
+        event = (Event)intent.getSerializableExtra("Event");
+        personManager = ManagerFactory.getPersonManager(this);
+        person = personManager.getAllPersonsOfEvent(event.getId());
+        personlist = new ArrayList<>();
+        map = new HashMap<String, Long>();
+        for (int i = 0; i < 10; i++){
+            personlist.add(person.get(i).getName());
+            map.put(person.get(i).getName(), person.get(i).getId());
+        }
+         ***/
 
         String[] items = new String[] {"Alice", "Jenny", "Louis", "Jeff", "Sherry", "Aaron", "Chiao", "Ning", "Chia-i"};
         //Use a Spinner to select one value from items
@@ -59,11 +92,12 @@ public class BillPage extends Activity {
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                String billName = etBillName.getText().toString();
-                String ownerName = spinner.getSelectedItem().toString();
+                billName = etBillName.getText().toString();
+                ownerName = spinner.getSelectedItem().toString();
+                //billManager.insertBill(new Bill(billName,map.get(ownerName), 0, 1));
                 Intent i  = new Intent(((Dialog)dialog).getContext(), BillContentPage.class);
                 startActivity(i);
-                //Toast.makeText(getBaseContext(), "Bill Name: " +  billName + " Owner: " + ownerName, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getBaseContext(), "Bill Name: " +  billName + " Owner: " + ownerName, Toast.LENGTH_SHORT).show();
             }
         });
 
