3c97a15a029185a6c74bcb8b5d34045acbb58f70
Chiao Fu <joannelovecat@gmail.com>
2016-11-29 21:18:05 -0800

    cf - can add/delete friend when edit event

--- app/src/main/java/com/example/sherrychuang/splitsmart/Activity/EditEventPage.java
+++ app/src/main/java/com/example/sherrychuang/splitsmart/Activity/EditEventPage.java
@@ -1,11 +1,14 @@
 package com.example.sherrychuang.splitsmart.Activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -14,6 +17,8 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cunoraz.tagview.Tag;
+import com.cunoraz.tagview.TagView;
 import com.example.sherrychuang.splitsmart.R;
 import com.example.sherrychuang.splitsmart.data.Event;
 import com.example.sherrychuang.splitsmart.data.EventDate;
@@ -22,7 +27,10 @@ import com.example.sherrychuang.splitsmart.manager.EventManager;
 import com.example.sherrychuang.splitsmart.manager.ManagerFactory;
 import com.example.sherrychuang.splitsmart.manager.PersonManager;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by Chiao on 2016/11/28.
@@ -30,12 +38,17 @@ import java.util.Calendar;
 
 public class EditEventPage extends Activity {
     private Event event;
+    private List<Person> people;
+    private List<Integer> peopleStatus;
     private EventManager eventManager;
     private PersonManager personManager;
 
     private EditText eventNameView;
     private TextView startDateView;
     private TextView endDateView;
+    private EditText personNameView;
+    private EditText personEmailView;
+    private TagView peopleView;
 
     private DatePickerDialog.OnDateSetListener startDatePickListener = new DatePickerDialog.OnDateSetListener() {
         @Override
@@ -62,11 +75,31 @@ public class EditEventPage extends Activity {
         eventNameView = (EditText) findViewById(R.id.event_name);
         startDateView = (TextView) findViewById(R.id.start_date);
         endDateView = (TextView) findViewById(R.id.end_date);
+        peopleView = (TagView) findViewById(R.id.people);
 
         eventManager = ManagerFactory.getEventManager(this);
         personManager = ManagerFactory.getPersonManager(this);
+        people = personManager.getAllPersonsOfEvent(event.getId());
+        peopleStatus = new ArrayList<Integer>(Collections.nCopies(people.size(), 0));
 
-        // default show name of event
+        // show people of event
+        for(int i = 0; i < people.size(); i++) {
+            // add a new tag in page
+            Tag tempTag = new Tag(people.get(i).getName());
+            tempTag.layoutColor = Color.GRAY;
+            tempTag.isDeletable = true;
+            peopleView.addTag(tempTag);
+        }
+        peopleView.setOnTagDeleteListener(new TagView.OnTagDeleteListener() {
+            @Override
+            public void onTagDeleted(final TagView view, final Tag tag, final int position) {
+                view.remove(position);
+                peopleStatus.set(position, -1);
+                Toast.makeText(getApplicationContext(), "#people " + Integer.toString(people.size()), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // show name of event
         eventNameView.setText(event.getName());
         eventNameView.addTextChangedListener(new TextWatcher() {
             @Override
@@ -85,7 +118,7 @@ public class EditEventPage extends Activity {
             }
         });
 
-        // default show date of event
+        // show date of event
         startDateView.setText("Start Date: " + event.getStartDate().getMonth() + "/" + event.getStartDate().getDay());
         endDateView.setText("End Date: " + event.getEndDate().getMonth() + "/" + event.getEndDate().getDay());
         startDateView.setOnClickListener(new View.OnClickListener() {
@@ -107,6 +140,47 @@ public class EditEventPage extends Activity {
         ImageButton addFriendButton = (ImageButton) findViewById(R.id.add_person);
         addFriendButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
+                // show add friend dialog
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(EditEventPage.this);
+                LayoutInflater inflater= EditEventPage.this.getLayoutInflater();
+                View layout = inflater.inflate(R.layout.add_friend_dialog_layout, null);
+                alertDialogBuilder.setView(layout);
+//                alertDialogBuilder.setView(R.layout.add_friend_dialog_layout);
+                final AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.setTitle( "Add Friend" );
+                alertDialog.show();
+
+                personNameView = (EditText) layout.findViewById(R.id.person_name);
+                personEmailView = (EditText) layout.findViewById(R.id.person_email);
+
+                Button okButton = (Button) layout.findViewById(R.id.ok);
+                okButton.setOnClickListener(new View.OnClickListener(){
+                    public void onClick(View view){
+                        String tempName = personNameView.getText().toString();
+                        String tempEmail = personEmailView.getText().toString();
+                        if (tempName.length() > 0 && tempEmail.length() > 0) {
+                            // add a new tag in page
+                            Tag tempTag = new Tag(tempName);
+                            tempTag.layoutColor = Color.GRAY;
+                            tempTag.isDeletable = true;
+                            peopleView.addTag(tempTag);
+                            // add person into array
+                            people.add(new Person(tempName, tempEmail, event.getId()));
+                            peopleStatus.add(1);
+                        }
+                        Toast.makeText(getApplicationContext(), "#friend " + Integer.toString(people.size()), Toast.LENGTH_SHORT).show();
+                        alertDialog.dismiss();
+                    }
+                });
+
+                Button cancelButton = (Button) layout.findViewById(R.id.cancel);
+                cancelButton.setOnClickListener(new View.OnClickListener(){
+                    public void onClick(View view){
+                        Toast.makeText(getApplicationContext(), "add friend cancel", Toast.LENGTH_SHORT).show();
+                        alertDialog.cancel();
+                    }
+                });
+
                 Toast.makeText(getApplicationContext(), "add friend", Toast.LENGTH_SHORT).show();
             }
         });
@@ -114,7 +188,21 @@ public class EditEventPage extends Activity {
         Button okButton = (Button) findViewById(R.id.ok);
         okButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
+                // update event
                 eventManager.updateEvent(event);
+                // update people
+                for(int i = 0; i < people.size(); i++) {
+                    switch (peopleStatus.get(i)) {
+                        case 1:
+                            // add a new person
+                            personManager.insertPerson(people.get(i));
+                            break;
+                        case -1:
+                            // remove a person
+                            personManager.deletePerson(people.get(i).getId());
+                            break;
+                    }
+                }
                 Toast.makeText(getApplicationContext(), "OK!", Toast.LENGTH_SHORT).show();
                 Intent myIntent = new Intent(view.getContext(), MainActivity.class);
                 startActivityForResult(myIntent,0);
