3dab7350e8201bcdb51832043b1e91aa04d31959
Chihhung <aaronchlin1025@gmail.com>
2016-12-06 00:15:11 -0800

    cl - fix bug cause by poorly design db

--- app/src/main/java/com/example/sherrychuang/splitsmart/Activity/MainActivity.java
+++ app/src/main/java/com/example/sherrychuang/splitsmart/Activity/MainActivity.java
@@ -20,6 +20,7 @@ import java.util.*;
 import com.example.sherrychuang.splitsmart.data.*;
 import com.example.sherrychuang.splitsmart.manager.*;
 import com.example.sherrychuang.splitsmart.R;
+import com.example.sherrychuang.splitsmart.model.CreateSampleDB;
 import com.example.sherrychuang.splitsmart.model.DatabaseTest;
 
 /**
@@ -36,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        new DatabaseTest(getApplicationContext()).run();
+//        new CreateSampleDB(getApplicationContext()).EraseAll();
+//        new CreateSampleDB(getApplicationContext()).Run();
 
         eventListView = (ListView) findViewById(R.id.event_list);
         eventManager = ManagerFactory.getEventManager(this);
